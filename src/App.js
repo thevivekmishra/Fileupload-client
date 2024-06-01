28f6b76ac47dd6c2ad321c8b5ee1efff6b97eb44
thevivekmishra <vivekmishra89994@gmail.com>
2024-06-01 12:45:08 +0530

    fileupload-client

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { useRef, useState, useEffect } from 'react';
 import './App.css';
 import upload from './assets/upload.png';
 import { uploadFile } from './services/api.js';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   const [file, setFile] = useState('');
-  const [result, setResult] = useState(' ');
+  const [result, setResult] = useState('');
+  const [uploading, setUploading] = useState(false); // upload status state
 
   const fileInputRef = useRef();
 
@@ -13,15 +15,31 @@ function App() {
     fileInputRef.current.click();
   };
 
+  const copyToClipboard = () => {
+    if (result) {
+      navigator.clipboard.writeText(result);
+      toast.success('Link copied to clipboard!');
+    }
+  };
+
   useEffect(() => {
     const getImage = async () => {
       if (file) {
+        setUploading(true); // Set uploading to true when a file is selected
+
         const data = new FormData();
         data.append('name', file.name);
         data.append('file', file);
 
-        let response = await uploadFile(data);
-        setResult(response.path);
+        try {
+          let response = await uploadFile(data);
+          setResult(response.path);
+          toast.success('File uploaded successfully!'); 
+        } catch (error) {
+          toast.error('File upload failed.'); 
+        } finally {
+          setUploading(false); // Set uploading to false after the upload is complete
+        }
       }
     };
     getImage();
@@ -29,6 +47,7 @@ function App() {
 
   return (
     <div className="container">
+      <Toaster />
       <div className="box">
         <h1>Effortlessly Share Your Files!</h1>
         <p>Upload your file in just a click and receive a link to share instantly.</p>
@@ -43,11 +62,18 @@ function App() {
           style={{ display: 'none' }}
           onChange={(e) => setFile(e.target.files[0])}
         />
-        {result && (
-          <div className='link'>
-            <a href={result} target="_blank">{result}</a>
+        {uploading && ( 
+          <div className="uploading-message">
+            Uploading Please Wait...
           </div>
         )}
+        {result && (
+          <div className="link">
+            <a href={result} target="_blank" rel="noopener noreferrer" className='live-link'>{result}</a>
+            <button className="copy-button" onClick={copyToClipboard}>Copy Link</button>
+          </div>
+        )}
+         
       </div>
   
       <div className="credit">
@@ -55,7 +81,7 @@ function App() {
       </div>
     </div>
   );
-  
 }
 
 export default App;
+
